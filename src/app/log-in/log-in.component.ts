f137754ff6bd378f18fdc02606a3826cb3558e89
Kudashov_dv <kudashov@kursksu.ru>
2017-12-09 20:39:26 +0300

    добавлена верстка, компоненты, стили

---
+++ src/app/log-in/log-in.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'form-log-in',
+  templateUrl: './log-in.component.html',
+  styleUrls: ['./log-in.component.scss']
+})
+export class LogInComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'form-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.scss']
+})
+export class SignUpComponent implements OnInit {
+
+  public constructor() { }
+
+  public ngOnInit(): void {
+  }
+
+}
