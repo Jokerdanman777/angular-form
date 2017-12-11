d977020a34374150bb8b015b06ca7092f1bac1c6
Kudashov <kudashov@kursksu.ru>
2017-12-11 13:00:06 +0300

    Реализована работа с реактивными формами

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -8,9 +8,26 @@ import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@ang
 })
 export class SignUpComponent implements OnInit {
 
-  public constructor() { }
+  public formModel: FormGroup;
+
+  public constructor(
+    private _fb: FormBuilder
+  ) { }
 
   public ngOnInit(): void {
+    this.formModel = this._fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(4)]],
+      lastName: ['', [Validators.required, Validators.minLength(4)]],
+      email: ['', [Validators.required, Validators.minLength(4)]],
+      passwordGroup: this._fb.group({
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+        cpassword: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]]
+      })
+    });
+  }
+
+  public show(value: any): void {
+    console.log(value);
   }
 
 }
